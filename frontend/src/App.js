103c64369110c7e022b96c090b06b32068b3b64f
Vini Guimarães <niciusfg@gmail.com>
2022-10-14 11:49:28 -0700

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./App.scss";
 
 import DayList from "./components/DayList";
 import Appointment from "./components/Appointment";
-import daysData from "./components/__mocks__/days.json";
+// import daysData from "./components/__mocks__/days.json";
 import appointmentsData from "./components/__mocks__/appointments.json";
 
 export default function Application() {
   const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState(daysData);
+  const [days, setDays] = useState({});
   const [appointments, setAppointments] = useState(appointmentsData);
+
+  useEffect(() => {
+    axios.get("/spots").then((res) => {
+      console.log("res", res.data);
+      setDays(res.data);
+    });
+  });
+
   function bookInterview(id, interview) {
     console.log(id, interview);
     const isEdit = appointments[id].interview;
